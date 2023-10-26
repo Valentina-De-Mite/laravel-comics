7e7416676cc64a7c1e554e3657370b0a78192b34
Valentina De Mite <valesakura97@gmail.com>
2023-10-26 18:33:45 +0200

    added header partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,42 +15,7 @@
 </head>
 
 <body>
-    <header id="site_header">
-        <div class="container py-3">
-            <div class="d-flex justify-content-center ">
-
-                <div>
-                    <img src="{{Vite::asset('resources/images/dc-logo.png')}}" class="w-50" srcset="">
-                </div>
-                
-                <a class="navitem" href="#" aria-current="page"
-                >CHARACTERS</a
-                >
-                <a class="navitem" href="#">COMICS</a>
-                <a class="navitem" href="#">MOVIES</a>
-                <a class="navitem" href="#">TV</a>
-                <a class="navitem" href="#">COLLECTIBLES</a>
-                <a class="navitem" href="#">VIDEOS</a>
-                <a class="navitem" href="#">FANS</a>
-                <a class="navitem" href="#">NEWS</a>
-                <a class="navitem" href="#">SHOP</a>
-
-
-                <form class="d-flex" role="search">
-                    <input class="form-control  border border-0 border-bottom border border-primary text-end" type="search" placeholder="Search" aria-label="Search"> 
-                    <svg class="align-self-center" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#000000}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-                    
-                </form>
-            </div>
-        </div>
-       
-
-
-
-    
-
-    </header>
-
+    @include('partial.header')
 
 
 
@@ -65,24 +30,24 @@
     <div class="black">
        <div class="container series py-5 text-center">
            <div class="blue popup py-1 px-3 fw-medium text-center text-light fs-4">
-                        CURRENT SERIES
-                        </div>
-                        <!-- <div class="row-cols-sm-6 row">
+                CURRENT SERIES
+            </div>
+                <!-- <div class="row-cols-sm-6 row">
                     
-                        </div> -->
+                </div> -->
 
-                        <button class="btn btn-primary py-2 px-5 fs-5 mt-4 fw-medium">
-                        LOAD MORE
-                        </button>
-                    </div>
+                <button class="btn btn-primary py-2 px-5 fs-5 mt-4 fw-medium">
+                    LOAD MORE
+                </button>
+        </div>
                     
 
-                    <div class="blue fs-4 text-light py-3">
-                        <div class="container row m-auto justify-content-between align-items-center">
-                        </div>
+            <div class="blue fs-4 text-light py-3">
+                <div class="container row m-auto justify-content-between align-items-center">
+                </div>
                 
 
-                    </div>                          
+            </div>                          
     </div>
         
 
@@ -145,37 +110,37 @@
   </div>
     <div class="footer text-light py-5 dark">
             <div class="container row justify-content-between m-auto">
-            <div class="col-2">
-                <button class="btn btn-outline-primary py-2">SING UP NOW!</button>
-            </div>
-            <div class="col-4 d-flex align-items-end">
-                <h5 class="tb text-primary me-4">FOLLOW US</h5>
-                <img
-                class="me-2 icon"
-                src="../assets/img/footer-facebook.png"
-                alt=""
-                srcset=""
-                />
-                <img
-                class="icon me-2"
-                src="../assets/img/footer-twitter.png"
-                alt=""
-                srcset=""
-                />
-                <img
-                class="icon me-2"
-                src="../assets/img/footer-youtube.png"
-                alt=""
-                srcset=""
-                />
-                <img
-                class="icon me-2"
-                src="../assets/img/footer-pinterest.png"
-                alt=""
-                srcset=""
-                />
-                <img class="icon" src="../assets/img/footer-periscope.png" alt="" />
-            </div>
+                <div class="col-2">
+                    <button class="btn btn-outline-primary py-2">SING UP NOW!</button>
+                </div>
+                <div class="col-4 d-flex align-items-end">
+                    <h5 class="tb text-primary me-4">FOLLOW US</h5>
+                    <img
+                    class="me-2 icon"
+                    src="{{Vite::asset('resources/images/footer-facebook.png')}}"
+                    alt=""
+                    srcset=""
+                    />
+                    <img
+                    class="icon me-2"
+                    src="{{Vite::asset('resources/images/footer-twitter.png')}}"
+                    alt=""
+                    srcset=""
+                    />
+                    <img
+                    class="icon me-2"
+                    src="{{Vite::asset('resources/images/footer-youtube.png')}}"
+                    alt=""
+                    srcset=""
+                    />
+                    <img
+                    class="icon me-2"
+                    src="{{Vite::asset('resources/images/footer-pinterest.png')}}"
+                    alt=""
+                    srcset=""
+                    />
+                    <img class="icon" src="{{Vite::asset('resources/images/footer-periscope.png')}}" />
+                </div>
             </div>
     </div>
 
